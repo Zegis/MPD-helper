f26f953e4107e16a696322bf0ff3585f4fdcd421
Zegis <kjgm@poczta.onet.pl>
2014-03-31 13:35:46 +0200

    Started implementing job tree for Hu algorithm

---
+++ headers/node.h
@@ -0,0 +1,15 @@
+#ifndef NODE_H
+#define NODE_H
+
+class Job;
+
+class node{
+
+private:
+    Job *data;
+    node *left;
+    node *middle;
+    node *right;
+}
+
+#endif // NODE_H
---
+++ source/tree.cpp
@@ -0,0 +1,5 @@
+#include "headers/tree.h"
+
+Tree::Tree(){
+    root = 0;
+}
